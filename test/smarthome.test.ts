4846baa4575a65cb5bcf49026c08f1404ebcc44a
Lukas Kurz <lukas@shortdev.de>
2025-11-26 22:41:53 +0100

    Ignore local tests

--- test/smarthome.test.ts
+++ test/smarthome.test.ts
@@ -1,17 +1,21 @@
 import { auth, UserPassword } from "../src/client/auth/index.ts";
 import { FritzClient } from "../src/client/index.ts";
 import { listDevices } from "../src/smarthome.ts";
-import { password, username } from "./utils.ts";
+import { baseUrl, password, username } from "./utils.ts";
 
-await using client = new FritzClient("http://fritz.box").use(
-  auth(new UserPassword({ username, password })),
-);
-
-Deno.test("listDevices", async () => {
-  const list = await listDevices(client);
-  for (const device of list) {
-    console.log(
-      `Device: ${device.name} (${device.productName}) by ${device.manufacturer}`,
+Deno.test({
+  name: "listDevices",
+  ignore: baseUrl === undefined,
+  async fn() {
+    await using client = new FritzClient(baseUrl).use(
+      auth(new UserPassword({ username, password })),
     );
-  }
+
+    const list = await listDevices(client);
+    for (const device of list) {
+      console.log(
+        `Device: ${device.name} (${device.productName}) by ${device.manufacturer}`,
+      );
+    }
+  },
 });
